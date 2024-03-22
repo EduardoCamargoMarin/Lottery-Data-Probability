5172d3b878209caf51751a9e0f2eea4be06d38c9
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2024-03-22 13:22:46 -0300

    Add files via upload

--- Loterica.py
+++ Loterica.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+probability_chances = []
+
 for n1 in range(1, 55 + 1):
     for n2 in range(1 + n1, 56 + 1):
         for n3 in range(1 + n2, 57 + 1):
@@ -9,11 +11,14 @@ for n1 in range(1, 55 + 1):
                     for n6 in range(1 + n5, 60 + 1):
                         time.sleep(0.1)
                         resultado = [n1, n2, n3, n4, n5, n6]
-                        
                         random.shuffle(resultado)
+                        
+                        if 10 in [n1, n2, n3, n4, n5, n6] or 24 in [n1, n2, n3, n4, n5, n6]:
+                            notification = '================\nSequência armazenada!\n================'
+                            probability_chances.append(resultado)
+                            print(notification)
+                            
                         print(' '.join(map(str, resultado)))
                         
                         
-                        
-                        #random.shuffle(resultado)
-                        #
+                    
